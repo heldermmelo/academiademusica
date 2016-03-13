6486416c891b59672c0483af92134779580e4d25
heldermmelo <heldermmelo@gmail.com>
2016-03-13 09:10:13 -0300

    validacoes de tamanho

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,5 +1,5 @@
 class Usuario < ActiveRecord::Base
-	validates :nome, presence: true
-	validates :email, presence: true
+	validates :nome, presence: true, length: {maximum: 50}
+	validates :email, presence: true, length:{maximum: 255}
 
 end
--- test/models/usuario_test.rb
+++ test/models/usuario_test.rb
@@ -17,4 +17,14 @@ class UsuarioTest < ActiveSupport::TestCase
 		@usuario.email = "   "
 		assert_not @usuario.valid?
 	end
+	
+	test "email nao deve ser longo" do
+		@usuario.nome = "a" * 244 + "exemplo.com"
+		assert_not @usuario.valid?
+	end
+	test "nome nao deve ser longo" do
+		@usuario.nome = "a" * 51
+		assert_not @usuario.valid?
+	end
+
 end
