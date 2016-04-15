311ed31e80894f9451263d123f677dabaa96c72c
heldermmelo <heldermmelo@gmail.com>
2016-04-14 22:14:05 -0300

    login e logout terminados

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,4 +1,6 @@
 class Usuario < ActiveRecord::Base
+	attr_accessor :remember_token
+	before_save { self.email = email.downcase }
 	validates :nome, presence: true, length: {maximum: 50}
 	EXPRESSAO_REGULAR_VALIDA = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length:{maximum: 255},
@@ -7,4 +9,31 @@ class Usuario < ActiveRecord::Base
 	uniqueness: {case_sensitive: false}
 	has_secure_password
   	validates :password, presence: true, length: { minimum: 6 }
+
+	# Returns the hash digest of the given string.
+  def Usuario.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Retorna um token aleatório
+  def Usuario.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remember um usuario no database for use in persistent sessions.
+  def remember
+    self.remember_token = Usuario.new_token
+    update_attribute(:remember_digest, Usuario.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  def forget
+	update_attribute(:remember_digest, nil)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331123431) do
+ActiveRecord::Schema.define(version: 20160414234322) do
 
   create_table "users", force: :cascade do |t|
     t.string   "nome"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160331123431) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "usuarios", ["email"], name: "index_usuarios_on_email", unique: true
